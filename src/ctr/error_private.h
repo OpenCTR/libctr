23b528137393290e5fcb4a035aa239e84397f402
Alex Shaw <alex.shaw.as@gmail.com>
2015-09-23 16:26:16 -0500

    Update to use new find_package from OpenCTR

--- src/ctr/error_private.h
+++ src/ctr/error_private.h
@@ -1,27 +1,30 @@
 /*
+ *******************************************************************************
  * libctr - Library for Nintendo 3DS homebrew.
  *
- * Copyright (C) 2015 The OpenCTR Project.
+ * Copyright (C) 2015, OpenCTR Contributors.
  *
  * This file is part of libctr.
  *
- * libctr is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
+ * libctr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
  *
- * libctr is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * libctr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with libctr. If not, see <http://www.gnu.org/licenses/>.
- */
+ *******************************************************************************
+*/
 
 #ifndef __LIBCTR_ERROR_PRIVATE_H__
 #define __LIBCTR_ERROR_PRIVATE_H__
 
 /**
+ * @private
  * @file ctr/error_private.h
  * @brief Error API (Private).
  */
--- src/fs.c
+++ src/fs.c
@@ -1,22 +1,24 @@
 /*
+ *******************************************************************************
  * libctr - Library for Nintendo 3DS homebrew.
  *
- * Copyright (C) 2015 The OpenCTR Project.
+ * Copyright (C) 2015, OpenCTR Contributors.
  *
  * This file is part of libctr.
  *
- * libctr is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
+ * libctr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
  *
- * libctr is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * libctr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with libctr. If not, see <http://www.gnu.org/licenses/>.
- */
+ *******************************************************************************
+*/
 
 #include <fcntl.h>
 
